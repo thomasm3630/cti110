afb1f1ab15f4dc404574d30d534773612c6b55f5
thomasm3630 <166328438+thomasm3630@users.noreply.github.com>
2024-04-07 14:46:36 -0400

    Add files via upload

---
+++ P1LAB1_ThomasMarcus.py
@@ -0,0 +1,8 @@
+print("#Marcus Thomas")
+print("#04/07/2024")
+print("#PILAB1")
+print("#Message welcoming the user to this course")
+
+print("Marcus")
+print("Thomas")
+print("Hello, Marcus Thomas! Welcome to CTI-10")

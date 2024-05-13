b9880741aacb7a1ce52ad3b36ebe08980eb94bc9
thomasm3630 <166328438+thomasm3630@users.noreply.github.com>
2024-05-13 18:51:58 -0400

    Add files via upload

---
+++ P3HW2_ThomasMarcus.py
@@ -0,0 +1,20 @@
+#Marcus Thomas
+#04/21/24
+#P3HW2 
+#salary
+
+name = input("Enter employee's name: ")
+hours = float(input("Enter number of hours worked: "))
+payrate = float(input("Enter employee's pay rate: "))
+gross = hours * payrate
+overtime = (hours - 40)
+overtime_pay = ((overtime * 1.5) * payrate)
+Regular_pay = 40 * payrate
+grosspay = (overtime_pay + Regular_pay)
+
+print()
+print("Employee name:    ", name)
+print()
+print("Hours Worked  Pay Rate  OverTime     OverTime Pay   RegHour Pay   Gross Pay")
+print("------------------------------------------------------------------------------")
+print(hours,"        ",payrate,"    ",overtime,"        ",overtime_pay,"        $",Regular_pay,"        $",grosspay)

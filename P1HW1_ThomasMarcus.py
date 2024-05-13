750a46e3d1c187121e143a4f05c65ccd2678c96d
thomasm3630 <166328438+thomasm3630@users.noreply.github.com>
2024-05-13 18:38:48 -0400

    Add files via upload

---
+++ P1HW1_ThomasMarcus.py
@@ -0,0 +1,11 @@
+integer = 4
+anotherinteger = 5
+print("Enter integer:")
+print(integer)
+print("You entered:",integer)
+print(integer, "squared is 16")
+print("And", integer, "cubed is 64 !!")
+print("Enter another integer:")
+print(anotherinteger)
+print(integer, "+", anotherinteger,"is 9")
+print(integer, "*", anotherinteger,"is 20")

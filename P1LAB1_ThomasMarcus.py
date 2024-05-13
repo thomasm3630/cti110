e0ab2250482449581af1d009856645e89fba9b69
thomasm3630 <166328438+thomasm3630@users.noreply.github.com>
2024-05-13 18:40:42 -0400

    Add files via upload

---
+++ P1LAB1_ThomasMarcus.py
@@ -0,0 +1,15 @@
+#Marcus Thomas
+#04/14/2024
+#P1Lab1
+#Input and print
+
+print('Enter your first name:')
+
+firstname = input('Enter your first name:')
+
+print('Enter your last name:')
+
+lastname = input('Enter your last name:')
+
+print("Hello", firstname , lastname,  " ! Welcome to CTI-110")
+
